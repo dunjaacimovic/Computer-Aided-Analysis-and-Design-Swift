fd646b69d52ab1fd62424f5c7f5a6e034ca6cb8d
dunjaacimovic <dunjaacimovicc@gmail.com>
2019-10-26 17:50:54 +0200

    Project setup

---
+++ APR/APR/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  APR
+//
+//  Created by Dunja Acimovic on 26/10/2019.
+//  Copyright © 2019 Dunja Acimovic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ APR/APR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  APR
+//
+//  Created by Dunja Acimovic on 26/10/2019.
+//  Copyright © 2019 Dunja Acimovic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
