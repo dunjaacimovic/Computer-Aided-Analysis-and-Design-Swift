bfb0b9d715fd3fa012ac387d98caa745549189d3
dunjaacimovic <dunjaacimovicc@gmail.com>
2019-10-28 06:45:29 +0100

    Reorganize repository

---
+++ APR/First/Matrix.swift
@@ -0,0 +1,22 @@
+//
+//  Matrix.swift
+//  APR
+//
+//  Created by Dunja Acimovic on 26/10/2019.
+//  Copyright © 2019 Dunja Acimovic. All rights reserved.
+//
+
+import Cocoa
+
+struct Matrix {
+    var dimensionM: Int
+    var dimensionN: Int
+    var elements: [Double]
+}
+
+extension Matrix {
+
+    func readToMatrix() {
+        
+    }
+}
---
+++ APR/First/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  APR
+//
+//  Created by Dunja Acimovic on 27/10/2019.
+//  Copyright © 2019 Dunja Acimovic. All rights reserved.
+//
+
+import Foundation
+import FileKit
+//import AppKit
+//
+//let app = NSApplication.shared
+//let appDelegate = AppDelegate()
+//app.delegate = appDelegate
+//_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+
+//let path = Path("/Examples/M1.txt")
+//let fileHandle = path.fileHandleForReading
+//
+//let data = fileHandle?.availableData
+print(try String(contentsOfFile: "/Examples/M1.txt"))
+
+
